50c004509cadfc4c6812d4f404133a53d99af4b0
EddieDang <you@example.com>
2024-06-05 00:09:38 -0400

    First Commit

---
+++ src/GameManager.java
@@ -0,0 +1,47 @@
+import java.util.HashMap;
+
+public class GameManager {
+    private HashMap<String, Integer> board;
+    private int turn;
+
+    public GameManager() {
+        this.board = new HashMap<>();
+        this.board.put("Green", 3);
+        this.board.put("Yellow", 7);
+        this.board.put("Orange", 5);
+        this.turn = 1;
+    }
+
+    public void removePiece(String color, int amount){
+        try{
+            if(validateMove(color, amount)){
+                int old_value = this.board.get(color);
+                this.board.remove(color);
+                this.board.put(color, old_value - amount);
+                changePlayer();
+                System.out.println(amount + " " + color + " has been removed!");
+            }
+        }catch (Exception e){
+            System.out.println("Invalid color or amount! Try Again!");
+        }
+    }
+
+    public boolean validateMove(String color, int amount){
+        if(this.board.containsKey(color) && (amount <= this.board.get(color))){
+            return true;
+        }
+        return false;
+    }
+
+    public void changePlayer(){
+        if(this.turn == 1){
+            this.turn = 2;
+        }
+        this.turn = 2;
+    }
+
+    public void printBoard(){
+        this.board.forEach((key, value) -> System.out.println(key + ": " + value));
+    }
+}
+
